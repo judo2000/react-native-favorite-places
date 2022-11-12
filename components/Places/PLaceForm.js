e63c5a9b995f2b1b4c670d761d987126f3123156
Stephen Moore <judo2000@comcast.net>
2022-11-12 14:33:11 -0700

    Add a header button to go to the AddPlace screen

---
+++ components/Places/PLaceForm.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const PLaceForm = () => {
+  return (
+    <View>
+      <Text>The PLace Form</Text>
+    </View>
+  );
+};
+
+export default PLaceForm;
+
+const styles = StyleSheet.create({});
---
+++ screens/AddPlace.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import PLaceForm from '../components/Places/PLaceForm';
+
+const AddPlace = () => {
+  return <PLaceForm />;
+};
+
+export default AddPlace;
+
+const styles = StyleSheet.create({});
