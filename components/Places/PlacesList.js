555fdcd76fb1b1879275018548d74c05f59bc657
Stephen Moore <judo2000@comcast.net>
2022-11-12 13:53:02 -0700

    Initial commit

---
+++ components/Places/PlacesList.js
@@ -0,0 +1,34 @@
+import { StyleSheet, Text, View, Flatlist } from 'react-native';
+import PlaceItem from './PlaceItem';
+
+const PlacesList = ({ places }) => {
+  if (!places || places.length === 0) {
+    return (
+      <View style={styles.fallbackContainer}>
+        <Text style={styles.fallbackText}>
+          No places added yet - add a place to see it here
+        </Text>
+      </View>
+    );
+  }
+  return (
+    <Flatlist
+      data={places}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => <PlaceItem place={item} />}
+    />
+  );
+};
+
+export default PlacesList;
+
+const styles = StyleSheet.create({
+  fallbackContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  fallbackText: {
+    fontSize: 16,
+  },
+});
