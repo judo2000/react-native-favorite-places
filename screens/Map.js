a01fba376d402a8d207fcd96e1dd8a08cd4d2822
Stephen Moore <judo2000@comcast.net>
2022-11-13 14:55:47 -0700

    Display a marker on the map when pressed

--- screens/Map.js
+++ screens/Map.js
@@ -1,26 +1,47 @@
-import MapView, { Marker } from "react-native-maps";
-import { StyleSheet, View, Dimensions } from "react-native";
- 
+import MapView, { Marker } from 'react-native-maps';
+import { StyleSheet, View, Dimensions } from 'react-native';
+import { useState } from 'react';
+
 function Map() {
+  const [selectedLocation, setSelectedLocation] = useState();
+
   const region = {
     latitude: 37.78,
     longitude: -122.43,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   };
+
+  const selectLocationHandler = (event) => {
+    const lat = event.nativeEvent.coordinate.latitude;
+    const lng = event.nativeEvent.coordinate.longitude;
+
+    setSelectedLocation({ lat: lat, lng: lng });
+  };
+
   return (
     <MapView
       style={styles.map}
       initialRegion={region}
-    ></MapView>
+      onPress={selectLocationHandler}
+    >
+      {selectedLocation && (
+        <Marker
+          title='Picked Location'
+          coordinate={{
+            latitude: selectedLocation.lat,
+            longitude: selectedLocation.lng,
+          }}
+        />
+      )}
+    </MapView>
   );
 }
- 
+
 export default Map;
- 
+
 const styles = StyleSheet.create({
-  
   map: {
-    flex: 1
+    flex: 1,
   },
 });
