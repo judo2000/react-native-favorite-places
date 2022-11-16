e4cb0bf390416c92bd2f1fb3ba33fb474c9d7bd1
Stephen Moore <judo2000@comcast.net>
2022-11-16 13:19:08 -0700

    Install and initialize SQLite and install expo SpashScreen

--- App.js
+++ App.js
@@ -4,17 +4,44 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import AllPlaces from './screens/AllPlaces';
 import AddPlace from './screens/AddPlace';
-import Map from './screens/Map'
+import Map from './screens/Map';
 import IconButton from './components/UI/IconButton';
 import { Colors } from './constants/colors';
+import { useCallback, useEffect, useState } from 'react';
+import { init } from './util/database';
+import * as SplashScreen from 'expo-splash-screen';
+import { Text } from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [dbInitialized, setDbInitialized] = useState(false);
+  useEffect(() => {
+    const prepare = async () => {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        init();
+      } catch (error) {
+        console.warn(error);
+      } finally {
+        setDbInitialized(true);
+      }
+    };
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (dbInitialized) {
+      await SplashScreen.hideAsync();
+    }
+  }, [dbInitialized]);
+
+  if (!dbInitialized) return null;
+
   return (
     <>
       <StatusBar style='dark' />
-      <NavigationContainer>
+      <NavigationContainer onReady={onLayoutRootView}>
         <Stack.Navigator
           screenOptions={{
             headerStyle: { backgroundColor: Colors.primary500 },
@@ -44,7 +71,7 @@ export default function App() {
               title: 'Add a New Place',
             }}
           />
-          <Stack.Screen name="Map" component={Map} />
+          <Stack.Screen name='Map' component={Map} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
