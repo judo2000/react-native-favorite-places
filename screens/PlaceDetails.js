6c8afad3614bed3e577ce06c14a55f37878bd501
Stephen Moore <judo2000@comcast.net>
2022-11-17 14:31:32 -0700

    Complete PlaceDetails screen and update map to load the place location in read only view

--- screens/PlaceDetails.js
+++ screens/PlaceDetails.js
@@ -1,22 +1,48 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 import OutlinedButton from '../components/UI/OutlinedButton';
 import { Colors } from '../constants/colors';
+import { fetchPlaceDetails } from '../util/database';
 
-const PlaceDetails = ({ route }) => {
-  const showOnMapHandler = () => {};
+const PlaceDetails = ({ route, navigation }) => {
+  const [fetchedPlace, setFetchedPlace] = useState();
+
+  const showOnMapHandler = () => {
+    navigation.navigate('Map', {
+      initialLat: fetchedPlace.location.lat,
+      initialLng: fetchedPlace.location.lng,
+    });
+  };
 
   const selectedPlaceId = route.params.placeId;
 
-  useEffect(() => {}, [selectedPlaceId]);
+  useEffect(() => {
+    const loadPlaceData = async () => {
+      const place = await fetchPlaceDetails(selectedPlaceId);
+      setFetchedPlace(place);
+      navigation.setOptions({
+        title: place.title,
+      });
+    };
+
+    loadPlaceData();
+  }, [selectedPlaceId]);
+
+  if (!fetchedPlace) {
+    return (
+      <View style={styles.fallback}>
+        <Text>Loading place data...</Text>
+      </View>
+    );
+  }
 
   return (
     <ScrollView>
-      <Image style={styles.image} />
+      <Image style={styles.image} source={{ uri: fetchedPlace.imageUri }} />
       <View style={styles.locationContainer}>
         <View style={styles.addressContainer}>
-          <Text style={styles.address}>ADDRESS</Text>
+          <Text style={styles.address}>{fetchedPlace.address}</Text>
         </View>
         <OutlinedButton icon='map' onPress={showOnMapHandler}>
           View On Map
@@ -29,6 +55,11 @@ const PlaceDetails = ({ route }) => {
 export default PlaceDetails;
 
 const styles = StyleSheet.create({
+  fallback: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   image: {
     height: '35%',
     minHeight: 300,
